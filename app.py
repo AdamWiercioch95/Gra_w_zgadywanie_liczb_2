d40d17f86015eb92b197bf57d0e782e5e37f826d
Adam Wiercioch <adamwiercioch95@gmail.com>
2024-04-12 15:22:10 +0200

    modyfied app.py

---
+++ app.py
@@ -0,0 +1,45 @@
+
+
+def user_input():
+    possible_inputs = ["too big", "too small", "you win"]
+    while True:
+        print("[too big", "too small", "you win]")
+        user_answer = input().lower()
+        if user_answer in possible_inputs:
+            break
+        print("Please enter a valid input.")
+    return user_answer
+
+
+def guess_number():
+    print("Think of a number between 1 and 1000 and I will guess it in max. 10 tries.")
+    minimum = 0
+    maximum = 1000
+    user_answer = ''
+    while user_answer != 'you win':
+        guess = int((maximum - minimum) / 2) + minimum
+        print(f"I'm guessing: {guess}")
+        user_answer = user_input()
+        if user_answer == 'too big':
+            maximum = guess
+        elif user_answer == 'too small':
+            minimum = guess
+
+    print("I knew it!!")
+
+
+guess_number()
+
+
+
+
+
+
+print('''Pobierz odpowiedź z zestawu:
+- za dużo,
+- za mało,
+- zgadłeś''')
+x = input()
+
+if x == 'za dużo':
+   maximum = guess
